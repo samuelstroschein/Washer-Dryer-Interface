fc2149bd3735c1540f01f95a9e076f7a4af124a8
Benn Samuel Stroschein <35429197+bennsamuel@users.noreply.github.com>
2019-12-23 18:18:27 +0100

    updated files

--- noWiFi_ESP32/src/main.cpp
+++ noWiFi_ESP32/src/main.cpp
@@ -1,32 +1,10 @@
 #include <Arduino.h>
 
-// pins
-const int led = 15;
-const int ldr = 12;
-
-
-
 void setup() {
-  Serial.begin(9600);
-  pinMode(led, OUTPUT);
-  pinMode(ldr, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  int ldrStatus = analogRead(ldr);
-
-  Serial.println(ldrStatus);
-
-  delay(100);
-
-
-  
-  if (ldrStatus <= 40){
-    digitalWrite(led, HIGH);
-    Serial.println("LDR IS DARK, LED IS ON");
-  } else {
-    digitalWrite(led, LOW);
-    Serial.println("----");
-  }
-   
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello");
 }
