33f084633a51880167ef9c7e227ccba08ca7f42c
Benn Samuel Stroschein <35429197+bennsamuel@users.noreply.github.com>
2019-12-26 20:27:56 +0100

    led strip | added analogWrite

--- noWiFi_ESP32/include/led.h
+++ noWiFi_ESP32/include/led.h
@@ -1,4 +1,5 @@
 # include <Arduino.h>
+# include <analogWrite.h>
 
 class Led {
     private:
--- noWiFi_ESP32/src/main.cpp
+++ noWiFi_ESP32/src/main.cpp
@@ -3,17 +3,20 @@
 
 
 // PINS
-const int washerWorkingLedPin = 22;
-const int washerFinishedLedPin = 23;
+const int washerWorkingLedPin = 18;        //22
+const int washerFinishedLedPin = 5;        //23
 const int washerGreenLedSensorPin = 13;
 const int washerOrangeLedSensorPin = 12;
 
+
+/*
 // LED STRIP PINS
-const int greenPin1 = 10;
+const int greenPin1 = 18;
 const int redPin1 = 5;
 
 const int greenPin2 = 21;
 const int redPin2 = 19;
+*/
 
 
 // Int with working led pin and finished led pin
@@ -47,3 +50,4 @@ void loop() {
   }
 
 }
+
